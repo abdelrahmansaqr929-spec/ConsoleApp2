30ffacba336fdbb6be8603063c8050a86191308d
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-09-03 10:01:50 +0300

    answer_01

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region answer_01
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+            #endregion
+           
+
+        }
+    }
+}
