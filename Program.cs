ea5b3e43c645c08eee265724ff7f5593b11bd35f
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-09-03 10:06:41 +0300

    ansewr_02

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@
             double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine(prices[1]);
             #endregion
-           
+            #region answer_02
+            int[,] ShelfCopeis = { {3,5},{1,4} };
+            Console.WriteLine(ShelfCopeis[1,0]);
+            #endregion
 
         }
     }
